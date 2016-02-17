55a9c00748b169057589c2f756d7e70701a52dd8
Ryan Thompson <ryan@pyrocms.com>
2016-02-16 19:20:10 -0600

    Sexy backgrounds

---
+++ resources/config/design.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'backgrounds' => [
+        'theme::img/backgrounds/urban.jpg',
+        'theme::img/backgrounds/forest.jpg',
+        'theme::img/backgrounds/ravine.jpg',
+        'theme::img/backgrounds/landscape.jpg',
+        'theme::img/backgrounds/power-lines.jpg'
+    ]
+];
