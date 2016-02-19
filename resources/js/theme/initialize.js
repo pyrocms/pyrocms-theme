5f6ecb9c968e547dadcfc5875c47a66ee0da0349
Ryan Thompson <service@aiwebsystems.com>
2016-02-19 12:46:42 -0600

    Tooltips n stuff

--- resources/js/theme/initialize.js
+++ resources/js/theme/initialize.js
@@ -10,6 +10,9 @@ $(function () {
     // Initialize Bootstrap tooltips.
     $('[data-toggle="tooltip"]').tooltip();
 
+    // Initialize Bootstrap popovers.
+    $('[data-toggle="popover"]').popover();
+
     // Flush the footer to the bottom.
     $('#main').css('min-height', $(window).height() - $('#brand').outerHeight() - $('#footer').outerHeight());
 });
